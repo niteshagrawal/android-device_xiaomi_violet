241ec2312b372ad01944d6b887ebfdd7e215a5d5
raghavt20 <raghavtandon2000@gmail.com>
2021-12-09 01:42:10 +0530

    violet: sepolicy: Allow SElinux neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,6 +176,7 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
